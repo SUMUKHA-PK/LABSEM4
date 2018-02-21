d4e4a1b79f11a615bfcfb4d1b0412eada3f89410
16CO145 <16CO145.peekay@nitk.edu>
2018-02-21 08:15:44 +0530

    Another update

--- lab4/dinephil.c
+++ lab4/dinephil.c
@@ -1,91 +1,86 @@
-#include<stdio.h>
- 
-#define n 4
- 
-int compltedPhilo = 0,i;
- 
-struct fork{
-    int taken;
-}ForkAvil[n];
- 
-struct philosp{
-    int left;
-    int right;
-}Philostatus[n];
- 
-void goForDinner(int philID){ //same like threads concept here cases implemented
-    if(Philostatus[philID].left==10 && Philostatus[philID].right==10)
-        printf("Philosopher %d completed his dinner\n",philID+1);
-    //if already completed dinner
-    else if(Philostatus[philID].left==1 && Philostatus[philID].right==1){
-            //if just taken two forks
-            printf("Philosopher %d completed his dinner\n",philID+1);
- 
-            Philostatus[philID].left = Philostatus[philID].right = 10; //remembering that he completed dinner by assigning value 10
-            int otherFork = philID-1;
- 
-            if(otherFork== -1)
-                otherFork=(n-1);
- 
-            ForkAvil[philID].taken = ForkAvil[otherFork].taken = 0; //releasing forks
-            printf("Philosopher %d released fork %d and fork %d\n",philID+1,philID+1,otherFork+1);
-            compltedPhilo++;
-        }
-        else if(Philostatus[philID].left==1 && Philostatus[philID].right==0){ //left already taken, trying for right fork
-                if(philID==(n-1)){
-                    if(ForkAvil[philID].taken==0){ //KEY POINT OF THIS PROBLEM, THAT LAST PHILOSOPHER TRYING IN reverse DIRECTION
-                        ForkAvil[philID].taken = Philostatus[philID].right = 1;
-                        printf("Fork %d taken by philosopher %d\n",philID+1,philID+1);
-                    }else{
-                        printf("Philosopher %d is waiting for fork %d\n",philID+1,philID+1);
-                    }
-                }else{ //except last philosopher case
-                    int dupphilID = philID;
-                    philID-=1;
- 
-                    if(philID== -1)
-                        philID=(n-1);
- 
-                    if(ForkAvil[philID].taken == 0){
-                        ForkAvil[philID].taken = Philostatus[dupphilID].right = 1;
-                        printf("Fork %d taken by Philosopher %d\n",philID+1,dupphilID+1);
-                    }else{
-                        printf("Philosopher %d is waiting for Fork %d\n",dupphilID+1,philID+1);
-                    }
-                }
-            }
-            else if(Philostatus[philID].left==0){ //nothing taken yet
-                    if(philID==(n-1)){
-                        if(ForkAvil[philID-1].taken==0){ //KEY POINT OF THIS PROBLEM, THAT LAST PHILOSOPHER TRYING IN reverse DIRECTION
-                            ForkAvil[philID-1].taken = Philostatus[philID].left = 1;
-                            printf("Fork %d taken by philosopher %d\n",philID,philID+1);
-                        }else{
-                            printf("Philosopher %d is waiting for fork %d\n",philID+1,philID);
-                        }
-                    }else{ //except last philosopher case
-                        if(ForkAvil[philID].taken == 0){
-                            ForkAvil[philID].taken = Philostatus[philID].left = 1;
-                            printf("Fork %d taken by Philosopher %d\n",philID+1,philID+1);
-                        }else{
-                            printf("Philosopher %d is waiting for Fork %d\n",philID+1,philID+1);
-                        }
-                    }
-        }else{}
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+typedef struct {
+  int position;
+  int count;
+  sem_t *forks;
+  sem_t *lock;
+} params_t;
+
+void initialize_semaphores(sem_t *lock, sem_t *forks, int num_forks);
+void run_all_threads(pthread_t *threads, sem_t *forks, sem_t *lock, int num_philosophers);
+
+void *philosopher(void *params);
+void think(int position);
+void eat(int position);
+
+int main(int argc, char *args[])
+{
+  int num_philosophers = 5;
+
+  sem_t lock;
+  sem_t forks[num_philosophers];
+  pthread_t philosophers[num_philosophers];
+
+  initialize_semaphores(&lock, forks, num_philosophers);
+  run_all_threads(philosophers, forks, &lock, num_philosophers);
+  pthread_exit(NULL);
 }
- 
-int main(){
-    for(i=0;i<n;i++)
-        ForkAvil[i].taken=Philostatus[i].left=Philostatus[i].right=0;
- 
-    while(compltedPhilo<n){
-        /* Observe here carefully, while loop will run until all philosophers complete dinner
-        Actually problem of deadlock occur only thy try to take at same time
-        This for loop will say that they are trying at same time. And remaining status will print by go for dinner function
-        */
-        for(i=0;i<n;i++)
-            goForDinner(i);
-        printf("\nTill now num of philosophers completed dinner are %d\n\n",compltedPhilo);
-    }
- 
-    return 0;
+
+void initialize_semaphores(sem_t *lock, sem_t *forks, int num_forks)
+{
+  int i;
+  for(i = 0; i < num_forks; i++) {
+    sem_init(&forks[i], 0, 1);
+  }
+
+  sem_init(lock, 0, num_forks - 1);
+}
+
+void run_all_threads(pthread_t *threads, sem_t *forks, sem_t *lock, int num_philosophers)
+{
+  int i;
+  for(i = 0; i < num_philosophers; i++) {
+    params_t *arg = malloc(sizeof(params_t));
+    arg->position = i;
+    arg->count = num_philosophers;
+    arg->lock = lock;
+    arg->forks = forks;
+
+    pthread_create(&threads[i], NULL, philosopher, (void *)arg);
+  }
+}
+
+void *philosopher(void *params)
+{
+  int i;
+  params_t self = *(params_t *)params;
+
+  for(i = 0; i < 3; i++) {
+    think(self.position);
+
+    sem_wait(self.lock);
+    sem_wait(&self.forks[self.position]);
+    sem_wait(&self.forks[(self.position + 1) % self.count]);
+    eat(self.position);
+    sem_post(&self.forks[self.position]);
+    sem_post(&self.forks[(self.position + 1) % self.count]);
+    sem_post(self.lock);
+  }
+
+  think(self.position);
+  pthread_exit(NULL);
+}
+
+void think(int position)
+{
+  printf("Philosopher %d thinking...\n", position);
+}
+
+void eat(int position)
+{
+  printf("Philosopher %d eating...\n", position);
 }
