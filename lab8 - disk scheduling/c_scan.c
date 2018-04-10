8c4830630caf855b3021a4648188f7c651acd4b9
16CO145 <16CO145.peekay@nitk.edu.in>
2018-04-10 13:45:41 +0530

    Directory names changed

--- lab8 - disk scheduling/c_scan.c
+++ lab8 - disk scheduling/c_scan.c
@@ -1,4 +1,4 @@
-//SCAN ALGORITHM
+//C-SCAN ALGORITHM
 #include<stdio.h>
 void sort (int a[],int n);
 int main()
@@ -44,9 +44,10 @@ int main()
           j++;s+=abs(queue[i]-queue[i-1]);
         }
       if(queue[n-1]!=size){printf("The %d-th movement is from track %d to %d with total seek of: %d\n",++j,queue[n-1],size,abs(size-queue[n-1]));s+=abs(size-queue[n-1]);}
-      for(i=0;i<z;i++)
+      printf("The %d-th movement is from track %d to %d with total seek of: %d\n",++j,size,queue[0],abs(size-queue[0]));  // Circular movement from end to beginning query
+      for(i=1;i<z;i++)
       {
-        printf("The %d-th movement is from track %d to %d with total seek of: %d\n",j+1,queue[i],queue[i-1],abs(queue[i]-queue[i-1]));
+        printf("The %d-th movement is from track %d to %d with total seek of: %d\n",j+1,queue[i-1],queue[i],abs(queue[i]-queue[i-1]));
         j++;s+=abs(queue[i]-queue[i-1]);
       }
       if(queue[i]!=queue[z]){printf("The %d-th movement is from track %d to %d with total seek of: %d\n",++j,queue[i],queue[z],abs(queue[i]-queue[z]));s+=abs(queue[i]-queue[z]);}
@@ -74,12 +75,12 @@ int main()
           j++;s+=abs(queue[i]-queue[i-1]);
         }
       if(queue[0]!=0){printf("The %d-th movement is from track %d to %d with total seek of: %d\n",++j,queue[0],0,queue[0]);s+=queue[0];}
-      for(i=z+2;i<n;i++)
+      if(queue[n-1]!=size){printf("The %d-th movement is from track %d to %d with total seek of: %d\n",++j,size,queue[n-1],abs(size-queue[n-1]));s+=abs(size-queue[n-1]);}
+      for(i=n-1;i>z;i--)
       {
-        printf("The %d-th movement is from track %d to %d with total seek of: %d\n",j+1,queue[i-1],queue[i],abs(queue[i]-queue[i-1]));
+        printf("The %d-th movement is from track %d to %d with total seek of: %d\n",j+1,queue[i],queue[i-1],abs(queue[i]-queue[i-1]));
         j++;s+=abs(queue[i]-queue[i-1]);
       }
-      if(queue[n-1]!=size){printf("The %d-th movement is from track %d to %d with total seek of: %d\n",j+1,queue[n-1],size,abs(size-queue[n-1]));s+=abs(queue[n-1]-size);}
       printf("\nFinal seek amount: %d\n",s);
     }
   return 0;
@@ -102,51 +103,46 @@ void sort (int a[],int n)
 }
 //-------------------------------------------------------------------TEST CASES----------------------------------------------------------------
 
-/*1*/
+/*
 
-//----Enter the number of requests:
-//8
-//----Enter the starting track of the seek pin:
-//53
-//----Enter the direction of motion of seek pin:
-//+
-//----Enter the end of the tracks(cylinder size):
-//199
-//----Enter the track position of all the requests:
-//98 14 37 122 64 124 67 183
-//----The 1-th movement is from track 53 to 64, with total seek of: 11
-//----The 2-th movement is from track 64 to 67 with total seek of: 3
-//----The 3-th movement is from track 67 to 98 with total seek of: 31
-//----The 4-th movement is from track 98 to 122 with total seek of: 24
-//----The 5-th movement is from track 122 to 124 with total seek of: 2
-//----The 6-th movement is from track 124 to 183 with total seek of: 59
-//----The 7-th movement is from track 183 to 199 with total seek of: 16
-//----The 8-th movement is from track 199 to 37 with total seek of: 162
-//----The 9-th movement is from track 37 to 14 with total seek of: 23
-//----The 10-th movement is from track 14 to 0 with total seek of: 14
+1.
 
-//----Final seek amount: 345
+Enter the number of requests: 8
+Enter the starting track of the seek pin: 53
+Enter the direction of motion of seek pin: +
+Enter the end of the tracks(cylinder size): 199
+Enter the track position of all the requests: 98 14 37 122 64 124 67 183
+The 1-th movement is from track 53 to 64, with total seek of: 11
+The 2-th movement is from track 64 to 67 with total seek of: 3
+The 3-th movement is from track 67 to 98 with total seek of: 31
+The 4-th movement is from track 98 to 122 with total seek of: 24
+The 5-th movement is from track 122 to 124 with total seek of: 2
+The 6-th movement is from track 124 to 183 with total seek of: 59
+The 7-th movement is from track 183 to 199 with total seek of: 16
+The 8-th movement is from track 199 to 14 with total seek of: 185
+The 9-th movement is from track 14 to 37 with total seek of: 23
 
-/*2*/
+Final seek amount: 169
 
-//----Enter the number of requests:
-// 8
-//----Enter the starting track of the seek pin:
-//53
-//----Enter the direction of motion of seek pin:
-//-
-//----Enter the end of the tracks(cylinder size):
-//199
-//----Enter the track position of all the requests:
-// 98 14 37 122 64 124 67 183
-//----The 1-th movement is from track 53 to 37, with total seek of: 16
-//----The 2-th movement is from track 37 to 14 with total seek of: 23
-//----The 3-th movement is from track 14 to 0 with total seek of: 14
-//----The 4-th movement is from track 0 to 67 with total seek of: 67
-//----The 5-th movement is from track 67 to 98 with total seek of: 31
-//----The 6-th movement is from track 98 to 122 with total seek of: 24
-//----The 7-th movement is from track 122 to 124 with total seek of: 2
-//----The 8-th movement is from track 124 to 183 with total seek of: 59
-//----The 9-th movement is from track 183 to 199 with total seek of: 16
 
-//----Final seek amount: 252
+2.
+
+Enter the number of requests: 8
+Enter the starting track of the seek pin: 53
+Enter the direction of motion of seek pin: -
+Enter the end of the tracks(cylinder size): 199
+Enter the track position of all the requests: 98 14 37 122 64 124 67 183
+The 1-th movement is from track 53 to 37, with total seek of: 16
+The 2-th movement is from track 37 to 14 with total seek of: 23
+The 3-th movement is from track 14 to 0 with total seek of: 14
+The 4-th movement is from track 199 to 183 with total seek of: 16
+The 5-th movement is from track 183 to 124 with total seek of: 59
+The 6-th movement is from track 124 to 122 with total seek of: 2
+The 7-th movement is from track 122 to 98 with total seek of: 24
+The 8-th movement is from track 98 to 67 with total seek of: 31
+The 9-th movement is from track 67 to 64 with total seek of: 3
+
+Final seek amount: 188
+
+
+*/
